0517ea6f06565f4adb5fa4359ccf7a15d305911f
George Tankersley <george.tankersley@gmail.com>
2018-02-27 00:00:00 +0000

    blake2s: minor cleanup and reorganization

--- blake2s.go
+++ blake2s.go
@@ -9,6 +9,7 @@ import (
 // The constant values will be different for other BLAKE2 variants. These are
 // appropriate for BLAKE2s.
 const (
+	// The length of the key field.
 	KeyLength = 32
 	// The maximum number of bytes to produce.
 	MaxOutput = 32
@@ -19,7 +20,7 @@ const (
 	// Number of G function rounds for BLAKE2s.
 	RoundCount = 10
 	// Size of a block buffer in bytes
-	BlockBytes = 64
+	BlockSize = 64
 
 	// Initialization vector for BLAKE2s
 	IV0 uint32 = 0x6a09e667
@@ -30,23 +31,6 @@ const (
 	IV5 uint32 = 0x9b05688c
 	IV6 uint32 = 0x1f83d9ab
 	IV7 uint32 = 0x5be0cd19
-)
-
-var (
-	// Lookup table of the permutations of 0...15 used by the BLAKE2 round
-	// function.
-	SIGMA = [10][16]uint32{
-		[16]uint32{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
-		[16]uint32{14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
-		[16]uint32{11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
-		[16]uint32{7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
-		[16]uint32{9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13},
-		[16]uint32{2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9},
-		[16]uint32{12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
-		[16]uint32{13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
-		[16]uint32{6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5},
-		[16]uint32{10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0},
-	}
 )
 
 // These are the user-visible parameters of a BLAKE2 hash instance. The
@@ -112,61 +96,11 @@ func initFromParams(p *parameterBlock) *Digest {
 
 	d := &Digest{
 		h:    []uint32{h0, h1, h2, h3, h4, h5, h6, h7},
-		buf:  make([]byte, 0, BlockBytes),
+		buf:  make([]byte, 0, BlockSize),
 		size: int(p.DigestSize),
 	}
 
 	return d
-}
-
-// Write adds more data to the running hash.
-func (d *Digest) Write(input []byte) (n int, err error) {
-	bytesWritten := 0
-
-	// If we have capacity, just copy and reslice to wait for a full block. If
-	// we don't have capacity, we'll need to take a full block and compress.
-	for bytesWritten < len(input) {
-		// How much space do we have left in the block?
-		freeBytes := cap(d.buf) - len(d.buf)
-		inputLeft := len(input) - bytesWritten
-
-		if inputLeft <= freeBytes {
-			oldOffset := len(d.buf)
-			newOffset := len(d.buf) + inputLeft
-			copy(d.buf[oldOffset:newOffset], input[bytesWritten:])
-			d.buf = d.buf[0:newOffset]
-			return bytesWritten + inputLeft, nil
-		}
-
-		oldOffset := len(d.buf)
-		newOffset := len(d.buf) + freeBytes
-		copy(d.buf[oldOffset:newOffset], input[bytesWritten:bytesWritten+freeBytes])
-		d.buf = d.buf[0:newOffset]
-
-		// increment counter, preserving overflow behavior
-		d.t0 += uint32(len(d.buf))
-		if d.t0 < uint32(len(d.buf)) {
-			d.t1++
-		}
-
-		// Before compressing, sanity-check the internal states.
-		// if len(d.buf) != cap(d.buf) || len(d.buf) != BlockBytes {
-		// 	return bytesWritten, errors.New("blake2s: tried to compress when buffer wasn't full")
-		// }
-		// if len(d.h) != 8 {
-		// 	return bytesWritten, errors.New("blake2s: internal hash state too large")
-		// }
-
-		d.compress()
-
-		// advance pointers
-		bytesWritten += freeBytes
-		d.buf = d.buf[:0]
-
-		// loop until we can't fill another buffer
-	}
-
-	return bytesWritten, nil
 }
 
 func (d *Digest) compress() {
@@ -326,6 +260,24 @@ func (d *Digest) compress() {
 	d.h[7] = d.h[7] ^ v7 ^ v15
 }
 
+// The internal BLAKE2s round function.
+func g(a, b, c, d, m1 uint32) (uint32, uint32, uint32, uint32) {
+	// We lift the table lookups and the initial triple addition into the
+	// caller so this function has a better chance of inlining. Similarly, the
+	// math/bits calls are themselves inlinable but seem to count against us in
+	// the AST budget anyway. TODO: file a bug for that
+
+	// a = a + b + m0
+	d = ((d ^ a) >> 16) | ((d ^ a) << (32 - 16))
+	c = c + d
+	b = ((b ^ c) >> 12) | ((b ^ c) << (32 - 12))
+	a = a + b + m1
+	d = ((d ^ a) >> 8) | ((d ^ a) << (32 - 8))
+	c = c + d
+	b = ((b ^ c) >> 7) | ((b ^ c) << (32 - 7))
+	return a, b, c, d
+}
+
 // Note that due to the nature of the hash.Hash interface, calling finalize
 // WILL NOT permanently update the underlying hash state. Instead it will
 // simulate what would happen if the current block were the final block.
@@ -349,14 +301,6 @@ func (d *Digest) finalize() ([]byte, error) {
 	// set last block flag
 	dCopy.f0 = 0xFFFFFFFF
 
-	// Before compressing, sanity-check the internal states.
-	// if len(dCopy.buf) != cap(dCopy.buf) || len(dCopy.buf) != BlockBytes {
-	// 	return nil, errors.New("blake2s: tried to compress when buffer wasn't full")
-	// }
-	// if len(dCopy.h) != 8 {
-	// 	return nil, errors.New("blake2s: internal hash state too large")
-	// }
-
 	dCopy.compress()
 
 	// extract output
@@ -366,24 +310,6 @@ func (d *Digest) finalize() ([]byte, error) {
 	}
 
 	return out, nil
-}
-
-// The internal BLAKE2s round function.
-func g(a, b, c, d, m1 uint32) (uint32, uint32, uint32, uint32) {
-	// We lift the table lookups and the initial triple addition into the
-	// caller so this function has a better chance of inlining. Similarly, the
-	// math/bits calls are themselves inlinable but seem to count against us in
-	// the AST budget anyway. TODO: file a bug for that
-
-	// a = a + b + m0
-	d = ((d ^ a) >> 16) | ((d ^ a) << (32 - 16))
-	c = c + d
-	b = ((b ^ c) >> 12) | ((b ^ c) << (32 - 12))
-	a = a + b + m1
-	d = ((d ^ a) >> 8) | ((d ^ a) << (32 - 8))
-	c = c + d
-	b = ((b ^ c) >> 7) | ((b ^ c) << (32 - 7))
-	return a, b, c, d
 }
 
 func NewDigest(key, salt, personalization []byte, outputBytes int) (*Digest, error) {
@@ -430,14 +356,56 @@ func NewDigest(key, salt, personalization []byte, outputBytes int) (*Digest, err
 
 	if key != nil {
 		// Write key to entire first block and compress
-		if len(key) < BlockBytes {
-			keyBuf := make([]byte, BlockBytes)
+		if len(key) < BlockSize {
+			keyBuf := make([]byte, BlockSize)
 			copy(keyBuf, key)
 			digest.Write(keyBuf)
 		}
 	}
 
 	return digest, nil
+}
+
+// Write adds more data to the running hash.
+func (d *Digest) Write(input []byte) (n int, err error) {
+	bytesWritten := 0
+
+	// If we have capacity, just copy and reslice to wait for a full block. If
+	// we don't have capacity, we'll need to take a full block and compress.
+	for bytesWritten < len(input) {
+		// How much space do we have left in the block?
+		freeBytes := cap(d.buf) - len(d.buf)
+		inputLeft := len(input) - bytesWritten
+
+		if inputLeft <= freeBytes {
+			oldOffset := len(d.buf)
+			newOffset := len(d.buf) + inputLeft
+			copy(d.buf[oldOffset:newOffset], input[bytesWritten:])
+			d.buf = d.buf[0:newOffset]
+			return bytesWritten + inputLeft, nil
+		}
+
+		oldOffset := len(d.buf)
+		newOffset := len(d.buf) + freeBytes
+		copy(d.buf[oldOffset:newOffset], input[bytesWritten:bytesWritten+freeBytes])
+		d.buf = d.buf[0:newOffset]
+
+		// increment counter, preserving overflow behavior
+		d.t0 += uint32(len(d.buf))
+		if d.t0 < uint32(len(d.buf)) {
+			d.t1++
+		}
+
+		d.compress()
+
+		// advance pointers
+		bytesWritten += freeBytes
+		d.buf = d.buf[:0]
+
+		// loop until we can't fill another buffer
+	}
+
+	return bytesWritten, nil
 }
 
 // Sum appends the current hash to b and returns the resulting slice.
@@ -462,4 +430,4 @@ func (d *Digest) Size() int { return d.size }
 // BlockSize returns the hash's underlying block size. The Write method must be
 // able to accept any amount of data, but it may operate more efficiently if
 // all writes are a multiple of the block size.
-func (d *Digest) BlockSize() int { return BlockBytes }
+func (d *Digest) BlockSize() int { return BlockSize }
