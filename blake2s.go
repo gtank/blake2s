805c20eab68d18690f89e62ebfefb67a21ff004c
George Tankersley <george.tankersley@gmail.com>
2018-08-25 00:00:00 +0000

    blake2s: remove now-unused sigma values

--- blake2s.go
+++ blake2s.go
@@ -32,23 +32,6 @@ const (
 	IV7 uint32 = 0x5be0cd19
 )
 
-var (
-	// Lookup table of the permutations of 0...15 used by the BLAKE2 round
-	// function.
-	SIGMA = [10][16]uint32{
-		[16]uint32{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
-		[16]uint32{14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
-		[16]uint32{11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
-		[16]uint32{7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
-		[16]uint32{9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13},
-		[16]uint32{2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9},
-		[16]uint32{12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
-		[16]uint32{13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
-		[16]uint32{6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5},
-		[16]uint32{10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0},
-	}
-)
-
 // These are the user-visible parameters of a BLAKE2 hash instance. The
 // parameter block is XOR'd with the IV at the beginning of the hash.
 // Currently we only support sequential mode, so many of these values will be
