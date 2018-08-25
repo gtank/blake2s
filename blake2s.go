fb678795229965fdfcedeec0721ec27d41992291
George Tankersley <george.tankersley@gmail.com>
2018-08-25 00:00:00 +0000

    blake2s: eliminate bounds checks in compress()
    Unroll all the loops and assign the frequently-used slice elements to
    local vars. As a side effect, this also removes two slow make/copies.
    
    name          old time/op    new time/op    delta
    Hash8Bytes-4     583ns ± 2%     418ns ± 2%  -28.37%  (p=0.000 n=8+8)
    Hash1K-4        5.29µs ± 3%    2.88µs ± 2%  -45.64%  (p=0.000 n=8+8)
    Hash8K-4        40.2µs ± 4%    21.3µs ± 3%  -47.05%  (p=0.000 n=8+8)
    
    name          old speed      new speed      delta
    Hash8Bytes-4  13.7MB/s ± 2%  19.1MB/s ± 2%  +39.53%  (p=0.000 n=8+8)
    Hash1K-4       194MB/s ± 3%   356MB/s ± 2%  +83.91%  (p=0.000 n=8+8)
    Hash8K-4       204MB/s ± 4%   385MB/s ± 3%  +88.83%  (p=0.000 n=8+8)

--- blake2s.go
+++ blake2s.go
@@ -175,55 +175,160 @@ func (d *Digest) compress() error {
 		return errors.New("blake2s: internal hash state too large")
 	}
 
-	// Split the buffer into 32-bit words
-	m := make([]uint32, 16)
-	for i := 0; i < 16; i++ {
-		m[i] = binary.LittleEndian.Uint32(d.buf[i*4 : i*4+4])
-	}
+	// Split the buffer into 16x32-bit words.
+	m0 := binary.LittleEndian.Uint32(d.buf[0*4 : 0*4+4])
+	m1 := binary.LittleEndian.Uint32(d.buf[1*4 : 1*4+4])
+	m2 := binary.LittleEndian.Uint32(d.buf[2*4 : 2*4+4])
+	m3 := binary.LittleEndian.Uint32(d.buf[3*4 : 3*4+4])
+	m4 := binary.LittleEndian.Uint32(d.buf[4*4 : 4*4+4])
+	m5 := binary.LittleEndian.Uint32(d.buf[5*4 : 5*4+4])
+	m6 := binary.LittleEndian.Uint32(d.buf[6*4 : 6*4+4])
+	m7 := binary.LittleEndian.Uint32(d.buf[7*4 : 7*4+4])
+	m8 := binary.LittleEndian.Uint32(d.buf[8*4 : 8*4+4])
+	m9 := binary.LittleEndian.Uint32(d.buf[9*4 : 9*4+4])
+	m10 := binary.LittleEndian.Uint32(d.buf[10*4 : 10*4+4])
+	m11 := binary.LittleEndian.Uint32(d.buf[11*4 : 11*4+4])
+	m12 := binary.LittleEndian.Uint32(d.buf[12*4 : 12*4+4])
+	m13 := binary.LittleEndian.Uint32(d.buf[13*4 : 13*4+4])
+	m14 := binary.LittleEndian.Uint32(d.buf[14*4 : 14*4+4])
+	m15 := binary.LittleEndian.Uint32(d.buf[15*4 : 15*4+4])
 
-	// New 16x32 array for updated hash state. Copy the current hash state to
-	// the top, then set IVs.
-	v := make([]uint32, 16)
-	copy(v, d.h)
-	v[8] = IV0
-	v[9] = IV1
-	v[10] = IV2
-	v[11] = IV3
-	v[12] = IV4 ^ d.t0
-	v[13] = IV5 ^ d.t1
-	v[14] = IV6 ^ d.f0
-	v[15] = IV7 ^ d.f1
+	// Create the internal round state. Copy the current hash state to the top,
+	// then the tweaked IVs to the bottom. Use local variables to avoid
+	// allocating another slice.
+	v0, v1, v2, v3 := d.h[0], d.h[1], d.h[2], d.h[3]
+	v4, v5, v6, v7 := d.h[4], d.h[5], d.h[6], d.h[7]
+	v8, v9, v10, v11 := IV0, IV1, IV2, IV3
+	v12 := IV4 ^ d.t0
+	v13 := IV5 ^ d.t1
+	v14 := IV6 ^ d.f0
+	v15 := IV7 ^ d.f1
 
-	for round := 0; round < RoundCount; round++ {
-		for i := 0; i < 8; i++ {
-			m0 := m[SIGMA[round][2*i+0]]
-			m1 := m[SIGMA[round][2*i+1]]
-			switch i {
-			case 0:
-				v[0], v[4], v[8], v[12] = g(v[0]+v[4]+m0, v[4], v[8], v[12], m1)
-			case 1:
-				v[1], v[5], v[9], v[13] = g(v[1]+v[5]+m0, v[5], v[9], v[13], m1)
-			case 2:
-				v[2], v[6], v[10], v[14] = g(v[2]+v[6]+m0, v[6], v[10], v[14], m1)
-			case 3:
-				v[3], v[7], v[11], v[15] = g(v[3]+v[7]+m0, v[7], v[11], v[15], m1)
-			case 4:
-				v[0], v[5], v[10], v[15] = g(v[0]+v[5]+m0, v[5], v[10], v[15], m1)
-			case 5:
-				v[1], v[6], v[11], v[12] = g(v[1]+v[6]+m0, v[6], v[11], v[12], m1)
-			case 6:
-				v[2], v[7], v[8], v[13] = g(v[2]+v[7]+m0, v[7], v[8], v[13], m1)
-			case 7:
-				v[3], v[4], v[9], v[14] = g(v[3]+v[4]+m0, v[4], v[9], v[14], m1)
-			default:
-				return errors.New("blake2s: invalid round index")
-			}
-		}
-	}
+	// This round structure is several steps removed from the spec and
+	// reference implementation. We unrolled the loops and calculated the
+	// offsets from the permutation table entry for each round, then directly
+	// mapped it to the correct word of the input block. This is a tradeoff:
+	// the doubly-indirect lookups were horrible for performance, but it's not
+	// at all obvious what this code is doing anymore.
 
-	for i := 0; i < 8; i++ {
-		d.h[i] = d.h[i] ^ v[i] ^ v[i+8]
-	}
+	// Round 0 w/ precomputed permutation offsets
+	v0, v4, v8, v12 = g(v0+v4+m0, v4, v8, v12, m1)
+	v1, v5, v9, v13 = g(v1+v5+m2, v5, v9, v13, m3)
+	v2, v6, v10, v14 = g(v2+v6+m4, v6, v10, v14, m5)
+	v3, v7, v11, v15 = g(v3+v7+m6, v7, v11, v15, m7)
+
+	v0, v5, v10, v15 = g(v0+v5+m8, v5, v10, v15, m9)
+	v1, v6, v11, v12 = g(v1+v6+m10, v6, v11, v12, m11)
+	v2, v7, v8, v13 = g(v2+v7+m12, v7, v8, v13, m13)
+	v3, v4, v9, v14 = g(v3+v4+m14, v4, v9, v14, m15)
+
+	// Round 1
+	v0, v4, v8, v12 = g(v0+v4+m14, v4, v8, v12, m10)
+	v1, v5, v9, v13 = g(v1+v5+m4, v5, v9, v13, m8)
+	v2, v6, v10, v14 = g(v2+v6+m9, v6, v10, v14, m15)
+	v3, v7, v11, v15 = g(v3+v7+m13, v7, v11, v15, m6)
+
+	v0, v5, v10, v15 = g(v0+v5+m1, v5, v10, v15, m12)
+	v1, v6, v11, v12 = g(v1+v6+m0, v6, v11, v12, m2)
+	v2, v7, v8, v13 = g(v2+v7+m11, v7, v8, v13, m7)
+	v3, v4, v9, v14 = g(v3+v4+m5, v4, v9, v14, m3)
+
+	// Round 2
+	v0, v4, v8, v12 = g(v0+v4+m11, v4, v8, v12, m8)
+	v1, v5, v9, v13 = g(v1+v5+m12, v5, v9, v13, m0)
+	v2, v6, v10, v14 = g(v2+v6+m5, v6, v10, v14, m2)
+	v3, v7, v11, v15 = g(v3+v7+m15, v7, v11, v15, m13)
+
+	v0, v5, v10, v15 = g(v0+v5+m10, v5, v10, v15, m14)
+	v1, v6, v11, v12 = g(v1+v6+m3, v6, v11, v12, m6)
+	v2, v7, v8, v13 = g(v2+v7+m7, v7, v8, v13, m1)
+	v3, v4, v9, v14 = g(v3+v4+m9, v4, v9, v14, m4)
+
+	// Round 3
+	v0, v4, v8, v12 = g(v0+v4+m7, v4, v8, v12, m9)
+	v1, v5, v9, v13 = g(v1+v5+m3, v5, v9, v13, m1)
+	v2, v6, v10, v14 = g(v2+v6+m13, v6, v10, v14, m12)
+	v3, v7, v11, v15 = g(v3+v7+m11, v7, v11, v15, m14)
+
+	v0, v5, v10, v15 = g(v0+v5+m2, v5, v10, v15, m6)
+	v1, v6, v11, v12 = g(v1+v6+m5, v6, v11, v12, m10)
+	v2, v7, v8, v13 = g(v2+v7+m4, v7, v8, v13, m0)
+	v3, v4, v9, v14 = g(v3+v4+m15, v4, v9, v14, m8)
+
+	// Round 4
+	v0, v4, v8, v12 = g(v0+v4+m9, v4, v8, v12, m0)
+	v1, v5, v9, v13 = g(v1+v5+m5, v5, v9, v13, m7)
+	v2, v6, v10, v14 = g(v2+v6+m2, v6, v10, v14, m4)
+	v3, v7, v11, v15 = g(v3+v7+m10, v7, v11, v15, m15)
+
+	v0, v5, v10, v15 = g(v0+v5+m14, v5, v10, v15, m1)
+	v1, v6, v11, v12 = g(v1+v6+m11, v6, v11, v12, m12)
+	v2, v7, v8, v13 = g(v2+v7+m6, v7, v8, v13, m8)
+	v3, v4, v9, v14 = g(v3+v4+m3, v4, v9, v14, m13)
+
+	// Round 5
+	v0, v4, v8, v12 = g(v0+v4+m2, v4, v8, v12, m12)
+	v1, v5, v9, v13 = g(v1+v5+m6, v5, v9, v13, m10)
+	v2, v6, v10, v14 = g(v2+v6+m0, v6, v10, v14, m11)
+	v3, v7, v11, v15 = g(v3+v7+m8, v7, v11, v15, m3)
+
+	v0, v5, v10, v15 = g(v0+v5+m4, v5, v10, v15, m13)
+	v1, v6, v11, v12 = g(v1+v6+m7, v6, v11, v12, m5)
+	v2, v7, v8, v13 = g(v2+v7+m15, v7, v8, v13, m14)
+	v3, v4, v9, v14 = g(v3+v4+m1, v4, v9, v14, m9)
+
+	// Round 6
+	v0, v4, v8, v12 = g(v0+v4+m12, v4, v8, v12, m5)
+	v1, v5, v9, v13 = g(v1+v5+m1, v5, v9, v13, m15)
+	v2, v6, v10, v14 = g(v2+v6+m14, v6, v10, v14, m13)
+	v3, v7, v11, v15 = g(v3+v7+m4, v7, v11, v15, m10)
+
+	v0, v5, v10, v15 = g(v0+v5+m0, v5, v10, v15, m7)
+	v1, v6, v11, v12 = g(v1+v6+m6, v6, v11, v12, m3)
+	v2, v7, v8, v13 = g(v2+v7+m9, v7, v8, v13, m2)
+	v3, v4, v9, v14 = g(v3+v4+m8, v4, v9, v14, m11)
+
+	// Round 7
+	v0, v4, v8, v12 = g(v0+v4+m13, v4, v8, v12, m11)
+	v1, v5, v9, v13 = g(v1+v5+m7, v5, v9, v13, m14)
+	v2, v6, v10, v14 = g(v2+v6+m12, v6, v10, v14, m1)
+	v3, v7, v11, v15 = g(v3+v7+m3, v7, v11, v15, m9)
+
+	v0, v5, v10, v15 = g(v0+v5+m5, v5, v10, v15, m0)
+	v1, v6, v11, v12 = g(v1+v6+m15, v6, v11, v12, m4)
+	v2, v7, v8, v13 = g(v2+v7+m8, v7, v8, v13, m6)
+	v3, v4, v9, v14 = g(v3+v4+m2, v4, v9, v14, m10)
+
+	// Round 8
+	v0, v4, v8, v12 = g(v0+v4+m6, v4, v8, v12, m15)
+	v1, v5, v9, v13 = g(v1+v5+m14, v5, v9, v13, m9)
+	v2, v6, v10, v14 = g(v2+v6+m11, v6, v10, v14, m3)
+	v3, v7, v11, v15 = g(v3+v7+m0, v7, v11, v15, m8)
+
+	v0, v5, v10, v15 = g(v0+v5+m12, v5, v10, v15, m2)
+	v1, v6, v11, v12 = g(v1+v6+m13, v6, v11, v12, m7)
+	v2, v7, v8, v13 = g(v2+v7+m1, v7, v8, v13, m4)
+	v3, v4, v9, v14 = g(v3+v4+m10, v4, v9, v14, m5)
+
+	// Round 9
+	v0, v4, v8, v12 = g(v0+v4+m10, v4, v8, v12, m2)
+	v1, v5, v9, v13 = g(v1+v5+m8, v5, v9, v13, m4)
+	v2, v6, v10, v14 = g(v2+v6+m7, v6, v10, v14, m6)
+	v3, v7, v11, v15 = g(v3+v7+m1, v7, v11, v15, m5)
+
+	v0, v5, v10, v15 = g(v0+v5+m15, v5, v10, v15, m11)
+	v1, v6, v11, v12 = g(v1+v6+m9, v6, v11, v12, m14)
+	v2, v7, v8, v13 = g(v2+v7+m3, v7, v8, v13, m12)
+	v3, v4, v9, v14 = g(v3+v4+m13, v4, v9, v14, m0)
+
+	d.h[0] = d.h[0] ^ v0 ^ v8
+	d.h[1] = d.h[1] ^ v1 ^ v9
+	d.h[2] = d.h[2] ^ v2 ^ v10
+	d.h[3] = d.h[3] ^ v3 ^ v11
+	d.h[4] = d.h[4] ^ v4 ^ v12
+	d.h[5] = d.h[5] ^ v5 ^ v13
+	d.h[6] = d.h[6] ^ v6 ^ v14
+	d.h[7] = d.h[7] ^ v7 ^ v15
 
 	return nil
 }
